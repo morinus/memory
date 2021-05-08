73560e19fb5e9427be79f37b1b98bc7d32984d20
Luka Mihaldinec <luka.mihaldinec@gmail.com>
2021-05-08 18:15:50 +0200

    [ADD] Added playerFactory

---
+++ MemoryGame/PlayerFactory.cpp
@@ -0,0 +1,26 @@
+#include "PlayerFactory.h"
+
+MemoryGame::PlayerFactory::PlayerFactory()
+{
+
+}
+
+MemoryGame::PlayerFactory::~PlayerFactory()
+{
+
+}
+
+std::vector<MemoryGame::Player> MemoryGame::PlayerFactory::CreatePlayers(int numberOfPlayers)
+{
+	std::vector<MemoryGame::Player> players;
+
+	for (int i = 0; i < numberOfPlayers; ++i)
+	{
+		Player newPlayer = Player("Player" + i + 1);
+		newPlayer.SetScore(0);
+
+		players.push_back(newPlayer);
+	}
+
+	return players;
+}
---
+++ MemoryGame/PlayerFactory.h
@@ -0,0 +1,20 @@
+#ifndef PLAYERFACTORY_H
+#define PLAYERFACTORY_H
+
+#include <vector>
+
+#include "Player.h"
+
+namespace MemoryGame
+{
+	class PlayerFactory
+	{
+	public:
+		PlayerFactory();
+		~PlayerFactory();
+
+		std::vector<Player> CreatePlayers(int numberOfPlayers);
+	};
+}
+
+#endif // PLAYERFACTORY_H
